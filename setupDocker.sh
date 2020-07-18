0a78dc39c9915007d693b28c7022604b59af415d
sarthakpranesh <sarthak.pranesh2018@vitstudent.ac.in>
2020-07-18 15:27:52 +0530

    Added script for setting up docker
    Signed-off-by: sarthakpranesh <sarthak.pranesh2018@vitstudent.ac.in>

---
+++ setupDocker.sh
@@ -0,0 +1,27 @@
+apt_wait () {
+  while sudo fuser /var/lib/dpkg/lock >/dev/null 2>&1 ; do
+    sleep 1
+  done
+  while sudo fuser /var/lib/apt/lists/lock >/dev/null 2>&1 ; do
+    sleep 1
+  done
+  if [ -f /var/log/unattended-upgrades/unattended-upgrades.log ]; then
+    while sudo fuser /var/log/unattended-upgrades/unattended-upgrades.log >/dev/null 2>&1 ; do
+      sleep 1
+    done
+  fi
+}
+
+sudo apt update
+apt_wait
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+apt_wait
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+apt_wait
+sudo apt update
+apt_wait
+sudo apt install docker-ce -y
+apt_wait
+sudo usermod -aG docker ${USER}
+su - ${USER}
