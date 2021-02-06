5f9229297402e6b2265d83ca67797686e913be45
sarthakpranesh <sarthak.pranesh2018@vitstudent.ac.in>
2021-02-06 14:31:22 +0530

    Updated golang installation script
    Signed-off-by: sarthakpranesh <sarthak.pranesh2018@vitstudent.ac.in>

--- golang.sh
+++ golang.sh
@@ -1,7 +1,7 @@
 echo "Download the golang amd64 package"
 # You can update this later when new release rolls out
-wget https://golang.org/dl/go1.15.5.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzf go1.15.5.linux-amd64.tar.gz
+wget https://golang.org/dl/go1.15.8.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.15.8.linux-amd64.tar.gz
 
 echo "Adding the go install location to bashrc"
 echo 'export GOROOT=/usr/local/go' >> ~/.bashrc
@@ -11,8 +11,10 @@ echo 'export PATH=$PATH:$GOROOT/bin' >> ~/.bashrc
 
 echo "Creating directories"
 # golib for local package install and Github/GO for active local development
-mkdir ~/golib
-mkdir ~/Github/GO
+mkdir -p ~/golib
+mkdir -p ~/Github/GO/src
+mkdir -p ~/Github/GO/bin
+mkdir -p ~/Github/GO/pkg
 
 echo "Adding the created directories to GOPATH"
 echo "export GOPATH=/home/$USER/golib" >> ~/.bashrc
@@ -20,10 +22,6 @@ echo 'export PATH=$PATH:$GOPATH/bin' >> ~/.bashrc
 
 echo "Creating compound GOPATH"
 echo "export GOPATH=\$GOPATH:/home/$USER/Github/GO" >> ~/.bashrc
-
-mkdir ~/Github/GO/src
-mkdir ~/Github/GO/bin
-mkdir ~/Github/GO/pkg
 
 rm -rf ./*.tar.gz
 
