6d6aef717de61cceb0a9ab711301c3cf72bd9142
sarthakpranesh <sarthak.pranesh2018@vitstudent.ac.in>
2021-02-07 15:22:07 +0530

    Updated fusuma config
    Signed-off-by: sarthakpranesh <sarthak.pranesh2018@vitstudent.ac.in>

--- nvm.sh
+++ nvm.sh
@@ -1,5 +1,5 @@
 echo "Installing NVM - Node Version Manager"
-sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
 [[ -s $HOME/.nvm/nvm.sh ]] && . $HOME/.nvm/nvm.sh
 
 echo "Using NVM to install latest version of Nodejs"
