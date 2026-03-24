ac0dde0d1a1ffeb254cf6aaaa94c46c0cb1f956b
Sarthak Pranesh <sarthakpranesh08@gmail.com>
2026-03-24 19:32:50 +0530

    chore: updated script for setting up go

--- golang.sh
+++ golang.sh
@@ -6,12 +6,13 @@ GO_VERSION=$1
 echo "Download the golang amd64 package"
 # You can update this later when new release rolls out
 if [ -z "$GO_VERSION" ]; then
-    GO_VERSION="1.17.3"
+    GO_VERSION="1.26.1"
 fi
 wget https://golang.org/dl/go$GO_VERSION.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go$GO_VERSION.linux-amd64.tar.gz
 
 echo "Adding the go install location to bashrc"
+echo '# Go' >> ~/.bashrc
 echo 'export GOROOT=/usr/local/go' >> ~/.bashrc
 echo 'export PATH=$PATH:$GOROOT/bin' >> ~/.bashrc
 
@@ -20,18 +21,19 @@ echo 'export PATH=$PATH:$GOROOT/bin' >> ~/.bashrc
 echo "Creating directories"
 # golib for local package install and Github/GO for active local development
 mkdir -p ~/golib
-mkdir -p ~/Github/GO/src/github.com
-mkdir -p ~/Github/GO/bin
-mkdir -p ~/Github/GO/pkg
+mkdir -p ~/work/github/sarthakpranesh/
+mkdir -p ~/work/github/go/bin
+mkdir -p ~/work/github/go/pkg
 
 echo "Adding the created directories to GOPATH"
 echo "export GOPATH=/home/$USER/golib" >> ~/.bashrc
 echo 'export PATH=$PATH:$GOPATH/bin' >> ~/.bashrc
 
 echo "Creating compound GOPATH"
-echo "export GOPATH=\$GOPATH:/home/$USER/Github/GO" >> ~/.bashrc
+echo "export GOPATH=\$GOPATH:/home/$USER/work/github/go" >> ~/.bashrc
 
 rm -rf ./*.tar.gz
 
 source ~/.bashrc
+
 go version
