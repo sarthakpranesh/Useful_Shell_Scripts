b15fc0fadde46e65b47d32f80b453d86462e5a47
sarthakpranesh <sarthak.pranesh@headout.com>
2026-07-17 10:43:27 +0530

    refactor: enhance golang installation script to support multiple architectures and improve shell configuration handling

--- golang.sh
+++ golang.sh
@@ -3,37 +3,60 @@
 # Get Go Version supplied by user
 GO_VERSION=$1
 
-echo "Download the golang amd64 package"
-# You can update this later when new release rolls out
 if [ -z "$GO_VERSION" ]; then
     GO_VERSION="1.26.1"
 fi
-wget https://golang.org/dl/go$GO_VERSION.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzf go$GO_VERSION.linux-amd64.tar.gz
 
-echo "Adding the go install location to bashrc"
-echo '# Go' >> ~/.bashrc
-echo 'export GOROOT=/usr/local/go' >> ~/.bashrc
-echo 'export PATH=$PATH:$GOROOT/bin' >> ~/.bashrc
+OS=$(uname -s | tr '[:upper:]' '[:lower:]')
+case "$(uname -m)" in
+    x86_64) GOARCH=amd64 ;;
+    arm64|aarch64) GOARCH=arm64 ;;
+    *) echo "Unsupported architecture: $(uname -m)"; exit 1 ;;
+esac
+GO_PKG="go${GO_VERSION}.${OS}-${GOARCH}.tar.gz"
+
+echo "Downloading Go ${GO_VERSION} for ${OS}-${GOARCH}"
+wget "https://golang.org/dl/${GO_PKG}"
+sudo tar -C /usr/local -xzf "$GO_PKG"
+
+GO_ENV=(
+    '# Go'
+    'export GOROOT=/usr/local/go'
+    'export PATH=$PATH:$GOROOT/bin'
+    "export GOPATH=$HOME/golib"
+    'export PATH=$PATH:$GOPATH/bin'
+    "export GOPATH=\$GOPATH:$HOME/work/github/go"
+)
+
+SHELL_RC=""
+if [ -f "$HOME/.bashrc" ]; then
+    SHELL_RC="$HOME/.bashrc"
+elif [ -f "$HOME/.zshrc" ]; then
+    SHELL_RC="$HOME/.zshrc"
+fi
+
+if [ -n "$SHELL_RC" ]; then
+    echo "Adding Go config to $SHELL_RC"
+    printf '%s\n' "${GO_ENV[@]}" >> "$SHELL_RC"
+else
+    echo "Config files not found (~/.bashrc, ~/.zshrc). Add the following to your shell config:"
+    printf '%s\n' "${GO_ENV[@]}"
+fi
 
 # Form here it is my personel preference of workflow
 
 echo "Creating directories"
 # golib for local package install and Github/GO for active local development
-mkdir -p ~/golib
-mkdir -p ~/work/github/sarthakpranesh/
-mkdir -p ~/work/github/go/bin
-mkdir -p ~/work/github/go/pkg
+mkdir -p "$HOME/golib"
+mkdir -p "$HOME/work/github/sarthakpranesh/"
+mkdir -p "$HOME/work/github/go/bin"
+mkdir -p "$HOME/work/github/go/pkg"
 
-echo "Adding the created directories to GOPATH"
-echo "export GOPATH=/home/$USER/golib" >> ~/.bashrc
-echo 'export PATH=$PATH:$GOPATH/bin' >> ~/.bashrc
+rm -f "$GO_PKG"
 
-echo "Creating compound GOPATH"
-echo "export GOPATH=\$GOPATH:/home/$USER/work/github/go" >> ~/.bashrc
-
-rm -rf ./*.tar.gz
-
-source ~/.bashrc
+if [ -n "$SHELL_RC" ]; then
+    # shellcheck source=/dev/null
+    source "$SHELL_RC"
+fi
 
 go version
